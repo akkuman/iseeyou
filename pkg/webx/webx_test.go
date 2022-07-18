0b4fc4e477531ffd857df82ea8ee06a9931fccac
Akkuman <akkumans@qq.com>
2022-07-18 15:36:26 +0800

    fix(test): 修正测试

--- pkg/webx/webx_test.go
+++ pkg/webx/webx_test.go
@@ -10,15 +10,16 @@ import (
 	"github.com/projectdiscovery/httpx/runner"
 )
 
+// go test -v ./pkg/webx/ -test.run TestWebXAct
 func TestWebXAct(t *testing.T) {
 	opt := options.Options{
 		WebXThreadCount: 50,
 	}
 	x := NewWebX(&opt)
 	inCh := make(chan interface{}, 10)
-	inCh <- portscan.IPPort{
-		IP: net.ParseIP("96.43.94.90"),
-		Port: 8081,
+	inCh <- &portscan.IPPort{
+		IP: net.ParseIP("36.112.99.182"),
+		Port: 39443,
 	}
 	close(inCh)
 	outCh := x.Act(context.Background(), inCh)
